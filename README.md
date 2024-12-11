6fc56fb1c502a84200565c8aace9030350d8d287
GiovaneCalado <giovane.calado@gmail.com>
2024-12-11 18:20:56 -0300

    UI exercise for BEON interview

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # beon-handson
+
+API Scenarios
+Scenario Outline:
+
+
+
+UI Scenarios - needs to be fixed
+Given access to Google Search Page
+When write a keyword in input field
+Then validate the search results contain the keyword
+
+Given access to Google Search Page
+When write a keyword in input field
+And click on the first result
+Then validate the page url matches the url shown on google page
---
+++ tests/example.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+const scenarios = [
+  // {keyword: 'Playwright'},
+  // {keyword: 'Beon'},
+  {keyword: 'Typescript'}
+]
+
+scenarios.forEach((scenario) => {
+  test(`Verify that search results are displayed on the search for ${scenario.keyword}`, async ({ page }) => {
+    await page.goto('https://google.com');
+    await page.locator('[name="q"]').fill(scenario.keyword);
+    await page.keyboard.press('Enter')
+    
+    const results = await page.locator('#search .g');
+    const resultsCount = await results.count();
+
+    let found = false
+    for(let i = 0; i < resultsCount; i++) {
+      const text = await results.nth(i).textContent()
+      if (text && text.toLowerCase().includes(scenario.keyword.toLowerCase())){
+        found = true;
+        break;
+      }
+    }
+    expect(found).toBeTruthy()
+  });
+
+});
+
+test(`verify that you are redirected to the correct URL after search`, async ({ page }) => {
+  await page.goto('https://google.com');
+  await page.locator('[name="q"]').fill('Beon Tech');
+  await page.keyboard.press('Enter')
+
+  const firstResultLink = page.locator('h3').first();
+  const href = await firstResultLink.locator('..').getAttribute('href')
+  console.log(href)
+  await firstResultLink.click()
+  await expect(page.locator('//*[@id="__next"]/main/div/div[1]/div[1]/div/div[1]/a/img')).toBeVisible();
+  expect(page.url()).toBe(href)
+});
+
